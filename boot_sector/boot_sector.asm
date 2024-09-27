f41ad715336052d433b894b8a62947ac4b3f6c94
Easter <ethinethin@gmail.com>
2024-09-27 19:00:45 -0400

    HELLO

--- boot_sector/boot_sector.asm
+++ boot_sector/boot_sector.asm
@@ -1,19 +1,19 @@
 [ORG 0x7c00]
 
-  MOV [BOOT_DRIVE], dl					; Copy boot drive (in dl) from BIOS to memory
+  MOV [BOOT_DRIVE], dl          ; Copy boot drive (in dl) from BIOS to memory
   MOV bp, 0x8000
   MOV sp, bp
 
-  MOV bx, 0x9000								; load 5 sectors to [0x0000 (ES) + 0x9000]
+  MOV bx, 0x9000                ; load 5 sectors to [0x0000 (ES) + 0x9000]
   MOV dh, 2
   MOV dl, [BOOT_DRIVE]
   CALL disk_load
 
-  MOV dx, [0x9000]							; print out the first loaded word
+  MOV dx, [0x9000]              ; print out the first loaded word
   CALL print_hex
   CALL print_newline
 
-  MOV dx, [0x9000 + 512]				; print out first word in the second sector
+  MOV dx, [0x9000 + 512]        ; print out first word in the second sector
   CALL print_hex
   CALL print_newline
   
--- boot_sector/disk_load.asm
+++ boot_sector/disk_load.asm
@@ -1,16 +1,16 @@
-disk_load:											; load dh sectors to es:bx from drive dl
+disk_load:                      ; load dh sectors to es:bx from drive dl
   PUSH dx
 
   ; Set up initial parameters
-  MOV ah, 0x02									; BIOS read sector function
-  MOV al, dh										; Read "dh" sectors, from:
-  MOV ch, 0x00									; Cylinder 0
-  MOV dh, 0x00									; Head 0
-  MOV cl, 0x02									; Start reading from the second sector (after the boot sector)
-  INT 0x13											; BIOS interrupt for reading
+  MOV ah, 0x02                  ; BIOS read sector function
+  MOV al, dh                    ; Read "dh" sectors, from:
+  MOV ch, 0x00                  ; Cylinder 0
+  MOV dh, 0x00                  ; Head 0
+  MOV cl, 0x02                  ; Start reading from the second sector (after the boot sector)
+  INT 0x13                      ; BIOS interrupt for reading
 
   ; Check for errors
-  JC disk_error								; carry flag set, error
+  JC disk_error                ; carry flag set, error
 
   ; Compare number of sectors read to number desired
   POP dx
