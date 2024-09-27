aaabf3492002c4571486701451d93b1d7f919e70
Easter <ethinethin@gmail.com>
2024-09-27 18:35:00 -0400

    Hello.

---
+++ boot_sector/boot_sector.asm
@@ -0,0 +1,34 @@
+[ORG 0x7c00]
+
+  MOV [BOOT_DRIVE], dl					; Copy boot drive (in dl) from BIOS to memory
+  MOV bp, 0x8000
+  MOV sp, bp
+
+  MOV bx, 0x9000								; load 5 sectors to [0x0000 (ES) + 0x9000]
+  MOV dh, 2
+  MOV dl, [BOOT_DRIVE]
+  CALL disk_load
+
+  MOV dx, [0x9000]							; print out the first loaded word
+  CALL print_hex
+  CALL print_newline
+
+  MOV dx, [0x9000 + 512]				; print out first word in the second sector
+  CALL print_hex
+  CALL print_newline
+  
+  jmp $
+
+%include "print.asm"
+%include "disk_load.asm"
+
+; Global variables
+BOOT_DRIVE: db 0
+
+; Boot sector padding
+TIMES 510-($-$$) DB 0
+DW 0xaa55
+
+; Disk space
+TIMES 256 DW 0x4141
+TIMES 256 DW 0x7373
---
+++ boot_sector/print.asm
@@ -0,0 +1,66 @@
+print_char:                     ; print the character at address bx
+  PUSHA
+  MOV ah, 0x0e
+  MOV al, [bx]
+  INT 0x10
+  POPA
+  RET
+
+print_string:                   ; print the string at address bx
+  PUSHA
+string_loop:
+  MOV al, [bx]
+  CMP al, 0
+  JE string_done
+  CALL print_char
+  ADD bx, 1
+  JMP string_loop
+string_done:
+  POPA
+  RET
+
+NEW_LINE: db 0x0A, 0x0D, 0
+
+print_newline:
+  PUSH bx
+  MOV bx, NEW_LINE
+  CALL print_string
+  POP bx
+  RET
+
+HEX_PREFIX: db "0x", 0
+HEX_CHARS: db "0123456789ABCDEF", 0
+
+print_hex:                      ; print the hex value in dx
+  PUSHA
+  ; output the hex prefix
+  MOV bx, HEX_PREFIX
+  CALL print_string
+  ; set up loop
+  MOV ah, 12
+print_hex_loop:
+  ; copy hex and bit shift right according to the shift index value ([bx])
+  MOV cx, dx
+  MOV al, ah
+print_hex_shift_loop:
+  CMP al, 0
+  JE print_hex_shift_done
+  SHR cx, 1
+  SUB al, 1
+  JMP print_hex_shift_loop
+print_hex_shift_done:
+  ; mask all but rightmost half-byte
+  AND cx, 0x000F
+  ; find appropriate character
+  MOV bx, HEX_CHARS
+  ADD bx, cx
+  CALL print_char
+  ; if current shift index is 0, we're done
+  CMP ah, 0
+  JE print_hex_done
+  SUB ah, 4
+  MOV al, ah
+  JMP print_hex_loop
+print_hex_done:
+  POPA
+  RET
